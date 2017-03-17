9b2488a961e83a74a4f53afbac4e1f38d11e0dea
C W <wahlers1@gmail.com>
2017-03-16 21:14:01 -0400

    Counter for log

---
+++ public/assets/js/time.js
@@ -0,0 +1,67 @@
+var goal = 0;
+var remaining = 0;
+var number = 0;
+var counter;
+var min = 0;
+var sec = 0;
+
+
+// $(".display-time").html("<h2>" + time + "</h2>");
+
+function run() {
+  if (goal == ""){
+    goal = $(".time-goal").val();
+    remaining = goal;
+  }
+  
+  $(".goal").html("<h2>Goal: " + goal + " Minutes</h2>");
+  $(".display-time").html("<h2>Remaining: " + remaining + " Minutes</h2>");
+  counter = setInterval(increment, 1000);
+}
+
+function increment() {
+  //  Increase number by one.
+  number++;
+
+  //  Show the number in the #show-number tag.
+  if (number == 60) {
+    number = 0;
+    min++;
+    remaining = remaining - min;
+    if (remaining < 0) {
+      var extra = min - goal;
+      $(".display-time").html("<h2>Extra: " + extra + " Minutes</h2>");
+    }else if (remaining == 1) {
+      $(".display-time").html("<h2>Remaining: " + remaining + " Minute</h2>");
+    }else{
+      $(".display-time").html("<h2>Remaining: " + remaining + " Minutes</h2>");
+    }
+  }
+  // min = (number/1000/60) << 0,
+  // sec = (number/1000) % 60;
+  //sec = number *100 / 60; 
+  // $(".display-time").html("<h2>" + time + "</h2>");
+}
+
+function stop() {
+  //  Clear "counter" interval.
+  clearInterval(counter);
+  //Call route /readers/post the data to the db
+
+}
+
+function pause() {
+  //  Clear "counter" interval.
+  clearInterval(counter);
+  $(".display-time").html("<h2>Remaining: " + remaining + " Minutes</h2>");
+}
+
+
+//  When the start button gets clicked, run the run function.
+$(".start").on("click", run);
+
+//  When the stop button gets clicked, run the stop function.
+$(".stop").on("click", stop);
+
+//  When the resume button gets clicked, execute the run function.
+$(".pause").on("click", pause);
